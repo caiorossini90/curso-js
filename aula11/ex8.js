43954a107f301272fa52f330c8a03550983cd232
Caio <caiorossini90@hotmail.com>
2023-01-05 18:13:18 -0300

    exercicios com node

---
+++ aula11/ex8.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade de seu carro é de ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida!')
+}
+console.log('Dirija sempre com cinto de segurança')
