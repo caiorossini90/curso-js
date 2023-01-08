d29cfd2c5db40c6facb9e96930723a394c753279
Caio <caiorossini90@hotmail.com>
2023-01-07 21:06:42 -0300

    functions

---
+++ aula16/funcao3.js
@@ -0,0 +1,20 @@
+/* function fatorial(n) {
+    let fat = 1
+    for(let c = n; c > 1; c--) {
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5)) */
+
+//RECURSIVIDADE
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(7))
